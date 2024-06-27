3c530f7e66e72bd7fdda462e28a357747393e088
Nelson Mbau <mbau.ndungu@gmail.com>
2024-06-27 13:26:44 +0300

    feat: add route methods

--- server/app.py
+++ server/app.py
@@ -21,5 +21,19 @@ def index():
     return make_response(body, 200)
 
 
+@app.route('/pets/<int:id>')
+def get_pet(id):
+    pet = Pet.query.get_or_404(id)
+    return make_response(pet.to_dict(), 200)
+
+@app.route('/species/<string:species>')
+def get_pets_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+    body = {
+        'pets': [pet.to_dict() for pet in pets],
+        'count': len(pets)
+    }
+    return make_response(body, 200)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
